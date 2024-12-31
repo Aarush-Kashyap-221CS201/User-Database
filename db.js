235bb350c8612ba0baf83ab465e0afaa5c35742e
Aarush Kashyap <aarushk1511@gmail.com>
2024-12-31 13:29:57 +0530

    Second commit

--- db.js
+++ db.js
@@ -1,11 +1,31 @@
+require('dotenv').config();
+
 const { Pool } = require('pg');
+const { Client } = require("pg");
+
+const SQL = `
+    CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR ( 255 ),
+    age INTEGER
+    );
+`;
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
 
 const pool = new Pool({
-    user: 'postgres', // Replace with your PostgreSQL username
-    host: 'localhost', // Replace with your database host
-    database: 'test', // Replace with your database name
-    password: 'aarushk1511', // Replace with your password
-    port: 5432, // Default PostgreSQL port
+    connectionString: process.env.DATABASE_URL
 });
+
+main();
 
 module.exports=pool;
